b06c125cc7cf9927b69721275ae0fa1d4f343694
rafaelkouhei <rafaelkouheisumiya@gmail.com>
2026-04-20 14:30:14 -0300

    Section 1.1.4 - Compound Procedures

---
+++ 1__book_notes/1__building_abstractions_with_procedures/1_1__the_elements_of_programming/1_1_4__compound_procedures.rkt
@@ -0,0 +1,27 @@
+#lang sicp
+
+; Procedure definition is the means to name a compound operation and treat them as a unit.
+; General form of a compound procedure:
+; (define (<name> <formal parameters>) (body))
+; name - symbol to be associated with the procedure definition in the environment.
+; formal parameters - names used within the body of the procedure to refer to the corresponding arguments of the procedure.
+; body - an expression that will yield the value of the procedure application when the formal parameters are replaced by the actual arguments to which the procedure is applied.
+
+(define (square x) (* x x))
+
+(square 21)
+; 441
+(square (+ 2 5))
+; 49
+(square (square 3))
+; 81
+
+; New compound procedures can be defined using the already existing procedure.
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+  
+(sum-of-squares 3 4)
+; 25
+
+; Compound procedures as used in the same way as primitive procedures.
+; In fact, there's no way to tell whether "square" is a built-in operation such as + or * without looking at its definition.
